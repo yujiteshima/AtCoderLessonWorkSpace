9fa561ed08bcbccdc252637f81c869987208d75b
Yuji Teshima <yuji.teshima@gmail.com>
2022-03-15 10:39:11 +0000

    1AC

---
+++ EveryDayAC/215.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main () {
+    string st;
+    cin >> st;
+    if(st == "Hello,World!"){
+        cout << "AC" << endl;
+    }
+    else
+    {
+        cout << "WA" << endl;
+    }
+    return 0;
+}
