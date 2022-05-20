44909a3f7244294866f84cbf463bdbcb5b13554c
Yuji Teshima <yuji.teshima@gmail.com>
2022-05-20 14:01:14 +0000

    1ac

---
+++ EveryDayAC/196_b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007; // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+
+int main () {
+  string x;
+  cin >> x;
+  string ans = "";
+  rep(i, x.size()) {
+    if (x[i] == '.') {
+      break;
+    }
+    ans.push_back(x[i]);
+  }
+  cout << ans << endl;
+  return 0;
+}
