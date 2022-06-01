20470a21a40037da8f38fcc2da709308a615fb29
Yuji Teshima <yuji.teshima@gmail.com>
2022-06-01 11:53:48 +0000

    1ac

---
+++ EveryDayAC/129_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+const int mod = 1'000'000'007; // mod(10^9 + 7)
+using Graph = vector<vector<int>>;
+template<class T> void chmin(T& a, T b) { if (a > b) a = b; }
+template<class T> void chmax(T& a, T b) { if (a < b) a = b; }
+
+int main () {
+  vector<int> A(3);
+  cin >> A[0] >> A[1] >> A[2];
+  sort(A.begin(), A.end());
+  cout << A[0] + A[1] << endl;
+  return 0;
+}
