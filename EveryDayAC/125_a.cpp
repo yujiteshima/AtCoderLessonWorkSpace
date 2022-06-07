a06942fdceafb4c3e509dbb866d41f09584425b0
Yuji Teshima <yuji.teshima@gmail.com>
2022-06-07 12:32:16 +0000

    2ac

--- EveryDayAC/125_a.cpp
+++ EveryDayAC/125_a.cpp
@@ -6,23 +6,20 @@ typedef long long ll;
 const ll INF = 1LL << 60;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repp(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
-#define ALL(a) (a).begin(), (a).end()
 const int mod = 1'000'000'007; // mod(10^9 + 7)
 using Graph = vector<vector<int>>;
 template<class T> void chmin(T& a, T b) { if (a > b) a = b; }
 template<class T> void chmax(T& a, T b) { if (a < b) a = b; }
 
 int main () {
-  int n, q;
-  cin >> n >> q;
-  vector<int> A(n);
-  rep(i, n) cin >> A[i];
-  sort(A.begin(), A.end());
-  rep(i, q) { 
-    int x;
-    cin >> x;
-    //auto itr = lower_bound(ALL(A), x);
-    cout << A.end() - lower_bound(ALL(A), x) << endl;
+  int a, b, t;
+  cin >> a >> b >> t;
+  int ans = 0;
+  repp(i, 1, t+1){
+      if(i % a == 0){
+        ans += b;
+      }
   }
+  cout << ans << endl;
   return 0;
 }
--- EveryDayAC/tempCodeRunnerFile.cpp
+++ EveryDayAC/tempCodeRunnerFile.cpp
@@ -1,6 +1,5 @@
-for (int i = x - 1; i < h; i++) {
-    //   if (S[i] == "#") {
-    //     break;
-    //   }
-    //   ans++;
-    // }
+rep(i, q) { ll x;
+  //   cin >> x;
+  //   auto itr = lower_bound(ALL(A), x);
+  //   cout << A.end() - itr << endl;
+  // }
